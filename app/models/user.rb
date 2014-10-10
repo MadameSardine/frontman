22c988533e9b5a6753b7a7c6fbac19bc14e4d05d
Sandrine <sandrine.zhangferron@gmail.com>
2014-10-10 16:42:02 +0100

    Pre-fill sign up form if passwords don't match

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User
 	property :id, 				Serial
 	property :name,				String
 	property :username,			String
-	property :email,			String
+	property :email,			String,		:unique => true
 	property :password_digest,	Text
 
 	def password=(password)
